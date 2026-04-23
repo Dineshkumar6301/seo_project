afe0aea9aba15e21ae25aacba4e07153ad5a5255
Dineshkumar6301 <dk967427@gmail.com>
2026-04-23 12:06:51 +0530

    update

--- seo/settings.py
+++ seo/settings.py
@@ -88,12 +88,15 @@ REST_FRAMEWORK = {
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+import dj_database_url
+import os
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 AUTHENTICATION_BACKENDS = [
