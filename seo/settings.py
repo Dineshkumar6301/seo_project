26530efc293d65e43ead61b7663fadeba4f50c25
Dineshkumar6301 <dk967427@gmail.com>
2026-05-25 16:05:12 +0530

    update

--- seo/settings.py
+++ seo/settings.py
@@ -73,21 +73,26 @@ REST_FRAMEWORK = {
     ]
 }
 
-import os
 import dj_database_url
-from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-
-DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.parse(
+            os.environ.get("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 AUTHENTICATION_BACKENDS = [
